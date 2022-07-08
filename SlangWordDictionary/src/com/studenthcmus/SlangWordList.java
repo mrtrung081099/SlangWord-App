22d9f6619768ea04e1adc66d7ac08138b6a1793f
Phạm Quang Trung <mrtrung081099@gmail.com>
2022-07-08 23:45:33 +0700

    create slang word class

---
+++ SlangWordDictionary/src/com/studenthcmus/SlangWordList.java
@@ -0,0 +1,15 @@
+package com.studenthcmus;
+import java.util.TreeMap;
+import java.util.regex.Pattern;
+
+
+
+public class SlangWordList {
+    public TreeMap<String, String> slangWords;
+    public int size;
+    
+    public SlangWordList() {
+        slangWords = new TreeMap<String, String>();
+        size = 0;
+    }
+}
