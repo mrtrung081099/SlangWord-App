ec617d45375f4bd153ee6aadfa2a5d1f1b419e88
Phạm Quang Trung <mrtrung081099@gmail.com>
2022-07-09 10:55:50 +0700

    update func slang class

--- SlangWordDictionary/src/com/studenthcmus/SlangWordList.java
+++ SlangWordDictionary/src/com/studenthcmus/SlangWordList.java
@@ -12,4 +12,26 @@ public class SlangWordList {
         slangWords = new TreeMap<String, String>();
         size = 0;
     }
+    public SlangWordList search(String definition) {
+        SlangWordList result = new SlangWordList();
+        for (String word : slangWords.keySet()) {
+            if (Pattern.compile(Pattern.quote(definition),
+                    Pattern.CASE_INSENSITIVE).matcher(slangWords.get(word)).find()) {
+                result.add(word, slangWords.get(word));
+            }
+        }
+        return result;
+    }
+
+    public String randomWord() {
+        int randomIndex = (int) (Math.random() * size);
+        int i = 0;
+        for (String word : slangWords.keySet()) {
+            if (i == randomIndex) {
+                return word;
+            }
+            i++;
+        }
+        return slangWords.keySet().iterator().next();
+    }
 }
